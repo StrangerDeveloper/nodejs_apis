8792aec8a31aeaae75bf4bab02c5d6edfa0ab6e3
Adnan ul Haq <adnan.haq@evampsaanga.com>
2024-05-31 15:12:02 +0500

    reverting back to mongoose

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotEnv = require("dotenv");
+dotEnv.config();
 const uri = process.env.MONGO_ATLAS_URI || '';
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const dotEnv = require("dotenv");
-const config = require('./config/config');
 const userRouter = require("./routes/user.router");
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGO_ATLAS_URI || '';
+const app = express();
 
 dotEnv.config();
 
-const port = process.env.PORT || 5000;
-const app = express();
-  // Middleware to parse JSON
+// Middleware to parse JSON
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -17,58 +18,54 @@ app.get("/", (req, res) => {
 
 
 
-const database = config.client;
-database.on('error', (error) => {
-  console.log(error)
-});
+//const database = config.client;
 
-app.use("/api", userRouter);
+//app.use("/api", userRouter);
 
-app.listen(port, () => {
-  console.log("Running on port 5000.");
-});
+// app.listen(port, () => {
+//   console.log("Running on port 5000.");
+// });
 
 // Export the Express API
 //module.exports = app;
 
-// app.use(
-//   express.urlencoded({
-//     extended: true,
-//   }));
-  // Middleware to parse JSON
+app.use(
+  express.urlencoded({
+    extended: true,
+  }));
+// Middleware to parse JSON
 //app.use(express.json());
 
-//const uri = process.env.MONGO_ATLAS_URI || '';
 
 //mongoose.connect(uri);
 
 
-//const database = mongoose.connection;
+const database = mongoose.connection;
 
 // Connect to MongoDB
-// mongoose.connect(uri).then(() => {
-//   console.log('Connected to MongoDB');
-//   app.get("/", (req, res) => res.send("Express on Vercel Connected"));
-// // Handle favicon.ico requests
-// app.get('/favicon.ico', (req, res) => res.status(204));
+mongoose.connect("mongodb+srv://adnan_es_node:qkDGpfDHjTQGCNgD@clusternodeapis.ouujwyu.mongodb.net/?retryWrites=true&w=majority&appName=ClusterNodeApis").then(() => {
+  console.log('Connected to MongoDB');
+ // app.get("/", (req, res) => res.send("Express on Vercel Connected"));
+  // Handle favicon.ico requests
+  app.get('/favicon.ico', (req, res) => res.status(204).send('favicon not found'));
 
-// app.use("/api", userRouter);
+  app.use("/api", userRouter);
 
-//   app.listen(port, () => {
-//     console.log(`Server is connected with port: ${port}`);
-//   });
-  
-// }).catch((err) => {
-//   console.error('Failed to connect to MongoDB', err);
-// });
+  app.listen(port, () => {
+    console.log(`Server is connected with port: ${port}`);
+  });
+
+}).catch((err) => {
+  console.error('Failed to connect to MongoDB', err);
+});
 
 // Export the Express API
 //module.exports = app;
 
 
-// database.on('error', (error) => {
-//   console.log(error)
-// });
+database.on('error', (error) => {
+  console.log(error)
+});
 
 // database.once('connected', () => {
 //   console.log('Database Connected');
